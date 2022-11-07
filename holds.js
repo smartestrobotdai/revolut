27bb0b4b48e8985b411d3058575a8a0773ee3318
Ken <smartestrobotdai@gmail.com>
2022-11-07 05:42:36 +0100

    -

---
+++ holds.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const { syncBuiltinESMExports } = require('module');
+const { mainModule } = require('process');
+var fsPromises = fs.promises;
+
+class Holds {
+  holds = []
+  path = ''
+  constructor(path) {
+    this.path = path
+  }
+  load() {
+    fsPromises.readFile(this.path).catch((e) => {
+
+      console.log(e)
+      if (e.code === 'ENOENT') {
+        return ''
+      } else {
+        throw e
+      }
+    }).then(content => {
+      content = "" + content
+      if (content) {
+        this.holds = JSON.parse(content)
+      }
+    })
+  }
+  
+  add(name, buyPrice) {
+    const index = this.holds.map(o => o.name).indexOf(name)
+    
+    if (index !== -1) {
+      this.holds[index] = {name, buyPrice}
+    } else {
+      this.holds.push({name, buyPrice})  
+    }
+
+    return fsPromises.writeFile(this.path, JSON.stringify(this.holds))
+  }
+  
+  remove(name) {
+    const index = this.holds.map(o => o.name).indexOf(name)
+    if (index !== -1) {
+      this.holds.splice(index, 1)
+      return fsPromises.writeFile(this.path, JSON.stringify(this.holds))
+    }
+    return Promise.resolve()
+  }
+  
+  getAll() {
+    return this.holds
+  }
+  
+  get(name) {
+    if (this.holds.length) {
+      const index = this.holds.map(o => o.name).indexOf(name)
+      if (index !== -1) {
+        return this.holds[index]
+      }
+    }
+    return null
+  }
+  
+  getPrice(name) {
+    const obj = this.get(name)
+    return obj && obj['buyPrice']
+  }
+
+  includes(name) {
+    const obj = this.get(name)
+    return obj !== null
+  }
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  holds = new Holds('myhold.json')
+  await holds.load()
+  await holds.add('MSFT', 200)
+  await holds.add('MSFT', 100)
+  await holds.add('AAPL', 150)
+  await holds.add('AAPL', 160)
+
+  await holds.remove('MSFT')
+  await sleep(100)
+  console.log(holds.getAll())
+  console.log(holds.getPrice('AAPL'))
+}
+
+//main()
+
+module.exports = {
+  Holds
+}
